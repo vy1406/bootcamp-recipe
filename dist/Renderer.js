fbfa6370a1d95f4d763dd2b14f7da56273204b5c
Sneguro4ka <yvova88@yahoo.com>
2019-04-04 13:14:00 +0300

    Dynammically adding button with functionality

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -8,5 +8,10 @@ class Renderer {
         $("#food").empty()
         let newHtml = template({ arrFoods })
         $("#food").append(newHtml)
+
+        $("img").on("click", function(){
+            const inputValue = $(this).closest(".singleFood").find(".ingredients")[0].children[0].innerText
+            console.log(inputValue)
+        })
     }
 }
